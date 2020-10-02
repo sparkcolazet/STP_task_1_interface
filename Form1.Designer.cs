8f169efa251ee0f46883f5bf9bd4c420a16dda47
sparkcolazet <sparkcolazet@gmail.com>
2020-10-03 00:58:57 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace STP_task_1_interface
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(33, 90);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(171, 90);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(33, 136);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(238, 37);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Do";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(69, 64);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(14, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "X";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(211, 64);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(15, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "N";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(451, 90);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 6;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(641, 90);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 7;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(451, 136);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(290, 37);
+			this.button2.TabIndex = 8;
+			this.button2.Text = "Do";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(487, 63);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(14, 13);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "X";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(677, 63);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(14, 13);
+			this.label4.TabIndex = 10;
+			this.label4.Text = "E";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(33, 208);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(114, 13);
+			this.label5.TabIndex = 11;
+			this.label5.Text = "Value of Math function";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(33, 240);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(108, 13);
+			this.label6.TabIndex = 12;
+			this.label6.Text = "Value of Taylor series";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(36, 278);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(96, 13);
+			this.label7.TabIndex = 13;
+			this.label7.Text = "Value of difference";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(451, 208);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(200, 13);
+			this.label8.TabIndex = 14;
+			this.label8.Text = "Value of sum terms that are bigger than e";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(451, 240);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(115, 13);
+			this.label9.TabIndex = 15;
+			this.label9.Text = "Quantity of those terms";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(454, 278);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(222, 13);
+			this.label10.TabIndex = 16;
+			this.label10.Text = "Value of sum terms that are bigger than e * 10";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(457, 320);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(168, 13);
+			this.label11.TabIndex = 17;
+			this.label11.Text = "Quantity of those terms with e * 10";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Enter += new System.EventHandler(this.Form1_Enter);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STP_task_1_interface
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+            Console.WriteLine("Input the argument x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input number of items");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double func = Math.Log(1 - x);
+            double d = Math.Abs(func - Sum(x, n)); //абсолютная прогрешность между разложением и функцией
+
+            Console.WriteLine("Value of Math function");
+            Console.WriteLine(func);
+            Console.WriteLine("Value of Taylor series");
+            Console.WriteLine(Sum(x, n));
+            Console.WriteLine("Value of difference");
+            Console.WriteLine(d);
+
+            Console.WriteLine("Input the argument x");
+
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input the argument e");
+            double E = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Value of sum terms that are bigger than e");
+            Console.WriteLine(FindSum(x, E));
+            Console.WriteLine("Quantity of those terms");
+            Console.WriteLine(FindTerm(x, E));
+            Console.WriteLine("Value of sum terms that are bigger than e * 10");
+            Console.WriteLine(FindSum(x, E * 10));
+            Console.WriteLine("Quantity of those terms with e * 10");
+            Console.WriteLine(FindTerm(x, E * 10));
+        }
+
+		
+
+        static double Sum(double x, int n) // посдчет через разложение в ряд
+        {
+            double sum = 0;
+            if (x < 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    sum += -Math.Pow(x, i) / i;
+                }
+                return sum;
+            }
+            else
+            {
+                Console.WriteLine("Недопустимые вводные данные");
+                Environment.Exit(0);
+                return 0;
+            }
+        }
+
+        static double FindSum(double x, double E)
+        {
+            double summary = 0;
+            int i = 1;
+            while (Math.Abs(-Math.Pow(x, i) / i) > E)
+            {
+                summary += Math.Abs(-Math.Pow(x, i) / i);
+                i++;
+            }
+
+            return summary;
+        }
+
+        static int FindTerm(double x, double E)
+        {
+            int i = 1;
+            while (Math.Abs(-Math.Pow(x, i) / i) > E)
+            {
+                i++;
+            }
+
+            return i--;
+        }
+
+		private void Form1_Enter(object sender, EventArgs e)
+		{
+            
+        }
+
+		private void textBox3_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+            double x = double.Parse(textBox1.Text);
+            int n = int.Parse(textBox2.Text);
+
+            double func = Math.Log(1 - x);
+            label5.Text = "Value of Math function " + func;
+
+            label6.Text = "Value of Taylor series " + Sum(x, n);
+
+            double d = Math.Abs(func - Sum(x, n));
+            label7.Text = "Value of difference " + d;
+
+        }
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+            double x = double.Parse(textBox4.Text);
+            double E = double.Parse(textBox5.Text);
+
+            label8.Text = "Value of sum terms that are bigger than e " + FindSum(x, E);
+
+            label9.Text = "Quantity of those terms " + FindTerm(x, E);
+
+            label10.Text = "Value of sum terms that are bigger than e * 10 " + FindSum(x, E * 10);
+            label11.Text = "Quantity of those terms with e * 10 " + FindTerm(x, E * 10);
+        }
+	}
+}
+
